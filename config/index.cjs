43f61d552a57e78eb86a49b91428555dd7b191ac
Han haiguang <hanhaiguang003@gmail.com>
2023-01-03 19:49:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -7,7 +7,7 @@
  */
 const USER_CONFIG = {
 
-  USE_PASSAGE: 'push-deer',
+  USE_PASSAGE: 'push-plus',
   // 使用微信测试号：公众号APP_ID
   APP_ID: '',
 
@@ -20,9 +20,9 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '测试',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU19111TLPXLdiLv0DIJ5YVUDqcFw7WbNr2uYW7I',
+      id: '68abb45ed31e4ef297945a6eaa5efe0d',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
